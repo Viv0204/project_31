569d54db289468e927cbee86fa532ebc9eca9bc3
Ivia08 <100661098+Viv0204@users.noreply.github.com>
2022-07-14 19:39:13 -0700

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -49,7 +49,7 @@ class PlayerArrow {
            at all the positions stored in trajectory array
            *******/
            for (var i = 0; i < this.trajectory.length; i++) {
-            ellipse(5, 5, this.trajectory[i][0], this.trajectory[i][1]);
+            ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5);
           }
      
   
